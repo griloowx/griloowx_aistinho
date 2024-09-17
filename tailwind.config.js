e95928f0cbb45c88ee5729e8d0fb24c0e142ed25
jhonnyssom <jhonnyssomux@gmail.com>
2024-09-17 11:49:04 -0300

    Add new files and directories

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetbrains: ['"JetBrains Mono"', 'monospace'], // Adicione a fonte personalizada aqui
+      },
+    },
+  },
+  plugins: [],
+}
